2d3aaef88780e18c127e801b564b6933b1138510
SADmammoth <lo.ma200018@gmail.com>
2019-02-26 18:12:00 +0300

    Added function checking is brackets correct in a given sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let stack = [];
+    let brace;
+
+    for(let i = 0; i < str.length; i++){
+
+        if(stack.length != 0){
+
+            brace = find_second_brace(bracketsConfig, str.charAt(i));
+
+            if(stack[stack.length-1] == brace){
+                stack.pop();
+                continue;
+            }
+            else{
+                return false;
+            }
+       }
+
+        brace = find_first_brace(bracketsConfig, str.charAt(i));
+
+       if(brace == -1){         
+          return false;
+       }
+
+        stack.push(str.charAt(i));
+    }
+
+    return stack.length == 0;
+}
+
+
+/*Function searching the opening brace in bracketsConfig*/
+function find_first_brace(array, char){
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i][0] == char){
+        return array[i][0];
+    }
+  }
+
+  return -1;
+}
+
+
+/*Function searching the closing brace in bracketsConfig*/
+function find_second_brace(array, char){
+
+    for(let i = 0; i < array.length; i++){
+      if(array[i][1] == char){
+          return array[i][0];
+      }
+  }
+
+  return -1;
 }
